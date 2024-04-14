e2a185cc2276f717e2a21430d9b002fb5319b5b4
Jacob Urbanczyk <jacob@urbanczyk.dev>
2024-04-14 15:38:07 +0200

    wip

---
+++ ci/benchmark_result_printer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import unittest
+import asyncio
+import argparse
+import json
+import re
+import sys
+import os
+import tabulate
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
